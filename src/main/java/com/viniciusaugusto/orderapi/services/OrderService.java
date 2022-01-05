5948bb6a850fab60f48d22ae97284679b4615d7b
Vinicius Augusto <vinicius_augusto_@msn.com>
2022-01-05 14:14:05 -0300

    Refactoring services interfaces and implementation

--- src/main/java/com/viniciusaugusto/orderapi/services/OrderService.java
+++ src/main/java/com/viniciusaugusto/orderapi/services/OrderService.java
@@ -1,39 +1,19 @@
 package com.viniciusaugusto.orderapi.services;
 
-
 import com.viniciusaugusto.orderapi.dto.OrderDTO;
 import com.viniciusaugusto.orderapi.entities.Order;
-import com.viniciusaugusto.orderapi.exceptions.OrderNotFoundException;
-import com.viniciusaugusto.orderapi.repositories.OrderRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 
-@Service
-public class OrderService {
+public abstract interface OrderService {
 
-    @Autowired
-    private OrderRepository repository;
+    public List<OrderDTO> findAll();
 
-    public List<OrderDTO> findAll() {
-        List<Order> list = repository.findAll();
-        return list.stream().map(OrderDTO::new).collect(Collectors.toList());
-    }
+    public OrderDTO findById(Long id);
 
-    public OrderDTO findById(Long id) {
-        return new OrderDTO(repository.findById(id).orElseThrow(() -> new OrderNotFoundException(id)));
-    }
+    public void insert(Order order);
 
-    public void insert(Order order) {
-        repository.save(order);
-    }
-
-    public void delete(Long id) {
-        repository.deleteById(id);
-    }
+    public void delete(Long id);
 }
 
 
---
+++ src/main/java/com/viniciusaugusto/orderapi/services/impl/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package com.viniciusaugusto.orderapi.services.impl;
+
+import com.viniciusaugusto.orderapi.dto.OrderDTO;
+import com.viniciusaugusto.orderapi.entities.Order;
+import com.viniciusaugusto.orderapi.exceptions.OrderNotFoundException;
+import com.viniciusaugusto.orderapi.repositories.OrderRepository;
+import com.viniciusaugusto.orderapi.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    @Override
+    public List<OrderDTO> findAll() {
+        List<Order> list = repository.findAll();
+        return list.stream().map(OrderDTO::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public OrderDTO findById(Long id) {
+        return new OrderDTO(repository.findById(id).orElseThrow(() -> new OrderNotFoundException(id)));
+    }
+
+    @Override
+    public void insert(Order order) {
+        repository.save(order);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
