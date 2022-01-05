23feb33b9aaeca24f98b5a0ecc464043eb7b536e
Vinicius Augusto <vinicius_augusto_@msn.com>
2022-01-05 14:23:43 -0300

    Refactoring swagger config location

--- src/main/java/com/viniciusaugusto/orderapi/OrderApiApplication.java
+++ src/main/java/com/viniciusaugusto/orderapi/OrderApiApplication.java
@@ -1,12 +1,7 @@
 package com.viniciusaugusto.orderapi;
 
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrderApiApplication {
@@ -14,14 +9,4 @@ public class OrderApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApiApplication.class, args);
 	}
-
-	@Bean
-	public OpenAPI customOpenApi() {
-		return new OpenAPI().info(new Info()
-				.title("")
-				.version("1.0")
-				.termsOfService("http://swagger.io/terms")
-				.license(new License().name("Apache 2.0").url("http://springdoc.org")));
-	}
-
 }
